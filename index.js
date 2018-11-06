7fb28df18010005697a5d9deeb5f68e877b593e0
NirobNabil <thetwin11100@gmail.com>
2018-11-06 21:28:50 +0600

    done with the entry animation + some of the 2nd screen

--- index.js
+++ index.js
@@ -11,17 +11,50 @@ document.addEventListener('scroll', function(){
 	console.log("gg");
 })
 
+function addClassTo(classArray, classToAdd, add){
+	let i = 0;
+	for(let i=0; i<classArray.length; i++){
+		let elems = document.querySelectorAll(classArray[i]);
+		for(let ii=0; ii<elems.length; ii++){
+			if(add){
+				console.log('adding ' + classToAdd[i] + ' to ' + classArray[i]);
+				elems[ii].classList.add(classToAdd[i]);	
+			}else if(!add){
+				elems[ii].classList.remove(classToAdd[i]);
+			}
+		}
+	}
+}
 
 document.addEventListener('DOMContentLoaded', function(){
 	///////////entry screen animations 
+	setTimeout(function(){
+		addClassTo(['.blackBack', '.whiteBack', '.hello', '.from'], ['blackBackVisible', 'whiteBackVisible', 'helloVisible', 'fromVisible'], true);
+	}, 100);
+})
 
-	////rubberband animation
+document.addEventListener('wheel', function(e){
 	let blackBack = document.getElementsByClassName('blackBack')[0];
 	let whiteBack = document.getElementsByClassName('whiteBack')[0];
 	let hello = document.getElementsByClassName('hello')[0];
 	let from = document.getElementsByClassName('from')[0];
-	blackBack.classList.add('blackBackVisible'); 
-	whiteBack.classList.add('whiteBackVisible');
-	hello.classList.add('helloVisible');
-	from.classList.add('fromVisible');
+	let BLACK = document.getElementsByClassName('BLACK')[0];
+	BLACK.style.width = 70 + 'vw';
+	document.querySelector('.rubberband').style.opacity = 0;
+	whiteBack.style.opacity = 0;
+	hello.style.opacity = 0;
+	from.style.opacity = 0;
+	blackBack.style.opacity = 0;
+	setTimeout(function(){
+		addClassTo(['.blackBack', '.whiteBack', '.hello', '.from'], ['blackBackVisible', 'whiteBackVisible', 'helloVisible', 'fromVisible'], false);
+		addClassTo(['.whatWeDoContainer', '.headline', '.primary .title', '.skill', '.features .box', '.skillset .title', '.secondary .title'], ['wwdVisible', 'visible', 'visible', 'visible', 'visible', 'visible', 'visible'], true);
+	}, 500)
 })
+
+
+
+
+
+
+
+
